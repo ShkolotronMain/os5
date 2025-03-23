74dce4ae884887e6bd2a8ab11e63bc34b41a48ab
ShkolotronMain <susbus5002@com>
2025-03-24 00:50:52 +0300

    YES THATS IT

---
+++ main.c
@@ -0,0 +1,110 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Общие данные
+#define BUF_SIZE 12
+int used = 0;
+int buffer[BUF_SIZE] = {0};
+
+
+// Выводит содержимое буфера и процент заполненности
+DWORD WINAPI buffer_control()
+{
+    int running = 1;
+    while (running)
+    {
+        // Вывод буфера
+        printf("\n[КОНТРОЛЬ] Содержимое: [");
+        for (int i=0; i<BUF_SIZE; i++)
+        {
+            printf("%d", buffer[i]);
+            if (i != BUF_SIZE-1)
+                printf(", ");
+        }
+        printf("]\n");
+
+        // Процент заполненности
+        printf("[КОНТРОЛЬ] Буфер заполнен на %.0f%%\n", (used/(float)BUF_SIZE)*100);
+
+        Sleep(2000);
+    }
+
+    return 0;
+}
+
+DWORD WINAPI get_number()
+{
+    int running = 1;
+    while (running)
+    {
+        // Если в буфере хоть что-то есть
+        if (used)
+        {
+            // Ищем ближайшую к началу занятую ячейку
+            for (int i=0; i<BUF_SIZE; i++)
+            {
+                if (buffer[i])
+                {
+                    printf("\n[Потребитель]: считано число %d из ячейки %d\n",buffer[i], i);
+                    buffer[i] = 0;
+                    used--;
+                    break;
+                }
+            }
+        }
+
+        Sleep(810);
+    }
+
+    return 0;
+}
+
+DWORD WINAPI set_number()
+{
+    int running = 1;
+    // Поток работает
+    while (running)
+    {
+        // Если буфер не заполнен
+        if (used != BUF_SIZE)
+        {
+            int number = rand()/30 + 1;
+            // Ищем свободную ячейку
+            for (int i=BUF_SIZE-1; i>=0; i--)
+            {
+                if (!buffer[i])
+                {
+                    buffer[i] = number;
+                    used++;
+                    printf("\n[Производитель]: записано число %d в ячейку %d\n",number, i);
+                    break;
+                }
+            }
+        }
+
+        Sleep(800);
+    }
+    
+    return 0;
+}
+
+
+float main()
+{
+    // 0 - prod
+    // 1 - cons
+    // 2 - control
+    HANDLE threads[3];
+    DWORD ids[3];
+
+    threads[0] = CreateThread(NULL, 0, set_number, NULL, 0, &ids[0]);
+    threads[1] = CreateThread(NULL, 0, get_number, NULL, 0, &ids[1]);
+    threads[2] = CreateThread(NULL, 0, buffer_control, NULL, 0, &ids[2]);
+
+    WaitForMultipleObjects(3, threads, TRUE, INFINITE);
+
+    CloseHandle(threads[0]);
+    CloseHandle(threads[1]);
+    CloseHandle(threads[2]);
+}
